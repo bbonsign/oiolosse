c9e80ed466c5cd496b785da35969b16e4d350a4b
Brian Bonsignore <bbonsign@users.noreply.github.com>
2023-09-14 22:54:42 -0400

    add podman

--- configuration.nix
+++ configuration.nix
@@ -48,6 +48,22 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+      # For Nixos version > 22.11
+      #defaultNetwork.settings = {
+      #  dns_enabled = true;
+      #};
+    };
+  };
+
   services.keyd.enable = true;
   services.keyd.ids = [ "05ac:0273" ];
   services.keyd.settings = {
