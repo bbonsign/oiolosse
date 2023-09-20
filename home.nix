fdb654b03f53ea8c7f73f82ed82a1b7e0bfe05d9
Brian Bonsignore <bbonsign@users.noreply.github.com>
2023-09-20 00:01:17 -0400

    add direnv to home.nix

--- home.nix
+++ home.nix
@@ -134,6 +134,12 @@
     # usbutils # lsusb
   ];
 
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true; # see note on other shells below
+    nix-direnv.enable = true;
+  };
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
