787164e23c7cf8619b4a941312ed030533f1a218
Brian Bonsignore <bonsignore.brian@gmail.com>
2023-12-15 00:53:26 -0500

    feat: add vi and vim aliases for neovim

--- programs/neovim.nix
+++ programs/neovim.nix
@@ -4,6 +4,8 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
     withNodeJs = true;
     withPython3 = true;
     extraPackages = with pkgs; [ gcc rnix-lsp sumneko-lua-language-server ];
