8871210bc07dc5d4914e05f52cd4b4d3ac7a675a
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-03-11 17:32:44 -0400

    feat: add time to right of starship prompt

--- programs/starship.nix
+++ programs/starship.nix
@@ -5,9 +5,16 @@
     enable = true;
     settings = {
       add_newline = true;
+      right_format = "$time";
+
       line_break.disabled = false;
       aws.disabled = true;
       gcloud.disabled = true;
+      time = {
+        disabled = false;
+        format = " [$time]($style) ";
+        style = "bold blue";
+      };
 
       character = {
         success_symbol = "[λ](bold blue)";
