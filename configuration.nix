9db11d33014e601faff5afb3bf90083bf88574d5
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-03-12 23:48:43 -0400

    fix: correct path to laptop keyboard device

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,7 @@
         extraDefCfg = ''
           process-unmapped-keys  yes
           log-layer-changes      no
-          linux-dev              /dev/input/event11
+          linux-dev              /dev/input/by-id/usb-Apple_Inc._Apple_Internal_Keyboard___Trackpad_D3H835500E1F-if01-event-kbd
         '';
         config = builtins.readFile ./kanata.kbd;
       };
