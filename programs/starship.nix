bffe11e7499151ddf0523dbc07069e45ec25ee47
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-03-15 17:11:25 -0400

    adjust starship prompt with time on right

--- programs/starship.nix
+++ programs/starship.nix
@@ -5,7 +5,9 @@
     enable = true;
     settings = {
       add_newline = true;
-      right_format = "$time";
+      format =
+        "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$solidity$swift$terraform$typst$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$direnv$env_var$crystal$custom$sudo$cmd_duration$fill$time$line_break$jobs$battery$status$os$container$shell$character";
+      # right_format = "$time";
 
       line_break.disabled = false;
       aws.disabled = true;
@@ -13,7 +15,7 @@
       time = {
         disabled = false;
         format = " [$time]($style) ";
-        style = "bold blue";
+        style = "blue";
       };
 
       character = {
@@ -24,6 +26,7 @@
         symbol = " ";
         style = "#5e3f9e";
       };
+      fill.symbol = " ";
       haskell.symbol = " ";
       lua.symbol = " ";
       python.symbol = " ";
