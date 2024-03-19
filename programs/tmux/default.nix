239ccde5c110dbcd4424a4c5f02e89dc1b09ae89
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-03-18 22:20:30 -0400

    extend tmux config with key sequences and finalize status bbar

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -30,20 +30,17 @@
         '';
       }
 
-      # {
-      #   plugin = tmuxPlugins.mkTmuxPlugin {
-      #     pluginName = "tmux-sessionx";
-      #     version = "0.1";
-      #     src = pkgs.fetchFromGitHub {
-      #       owner = "omerxx";
-      #       repo = "tmux-sessionx";
-      #       rev = "e39372ad2a275d73b1680483608c4dbac5c1a219";
-      #       sha256 = "sha256-daV8D6d7JmjroYA7CBEjWGNu1KDCWafVGeYrLh8FAsg=";
-      #     };
-      #   };
-      # }
+      {
+        plugin = tmuxPlugins.fuzzback;
+        extraConfig = ''
+          set -g @fuzzback-table tableSearch
+          set -g @fuzzback-bind /
+        '';
+      }
 
       { plugin = tmuxPlugins.tmux-fzf; }
+
+      { plugin = tmuxPlugins.yank; }
 
       {
         # Bound to prefix-u be default
@@ -56,6 +53,8 @@
 
       { plugin = tmuxPlugins.yank; }
 
+      { plugin = tmuxPlugins.prefix-highlight; }
+
       {
         plugin = tmuxPlugins.resurrect;
         extraConfig = "set -g @resurrect-strategy-nvim 'session'";
@@ -66,36 +65,49 @@
         extraConfig = "set -g @continuum-restore 'on'";
       }
 
-      {
-        plugin = tmuxPlugins.catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_window_left_separator ""
-          set -g @catppuccin_window_right_separator " "
-          set -g @catppuccin_window_middle_separator "█ "
-          set -g @catppuccin_window_number_position "left"
-          set -g @catppuccin_window_default_fill "number"
-          set -g @catppuccin_window_default_text "#W"
-          set -g @catppuccin_window_current_fill "number"
-          set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,  ,}"
-          set -g @catppuccin_status_modules_right "directory date_time"
-          set -g @catppuccin_status_modules_left "session"
-          set -g @catppuccin_status_left_separator  " "
-          set -g @catppuccin_status_right_separator " "
-          set -g @catppuccin_status_right_separator_inverse "no"
-          set -g @catppuccin_status_fill "icon"
-          set -g @catppuccin_status_connect_separator "no"
-          set -g @catppuccin_directory_text "#{b:pane_current_path}"
-          set -g @catppuccin_date_time_text "%H:%M"
+      # {
+      #   plugin = tmuxPlugins.catppuccin;
+      #   extraConfig = ''
+      #     set -g @catppuccin_window_left_separator ""
+      #     set -g @catppuccin_window_right_separator " "
+      #     set -g @catppuccin_window_middle_separator "█ "
+      #     set -g @catppuccin_window_number_position "left"
+      #     set -g @catppuccin_window_default_fill "number"
+      #     set -g @catppuccin_window_default_text "#W"
+      #     set -g @catppuccin_window_current_fill "number"
+      #     set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,  ,}"
+      #     set -g @catppuccin_status_modules_right "directory date_time"
+      #     set -g @catppuccin_status_modules_left "session"
+      #     set -g @catppuccin_status_left_separator  " "
+      #     set -g @catppuccin_status_right_separator " "
+      #     set -g @catppuccin_status_right_separator_inverse "no"
+      #     set -g @catppuccin_status_fill "icon"
+      #     set -g @catppuccin_status_connect_separator "no"
+      #     set -g @catppuccin_directory_text "#{b:pane_current_path}"
+      #     set -g @catppuccin_date_time_text "%H:%M"
+      #
+      #     set -g @catppuccin_icon_window_last "󰖰"
+      #     set -g @catppuccin_icon_window_current ""
+      #     set -g @catppuccin_icon_window_zoom "󰁌 "
+      #     set -g @catppuccin_icon_window_mark "󰃀"
+      #     set -g @catppuccin_icon_window_silent "󰂛 "
+      #     set -g @catppuccin_icon_window_activity "󰂞 "
+      #     set -g @catppuccin_icon_window_bell "󰂞 "
+      #   '';
+      # }
 
-          set -g @catppuccin_icon_window_last "󰖰"
-          set -g @catppuccin_icon_window_current ""
-          set -g @catppuccin_icon_window_zoom "󰁌 "
-          set -g @catppuccin_icon_window_mark "󰃀"
-          set -g @catppuccin_icon_window_silent "󰂛 "
-          set -g @catppuccin_icon_window_activity "󰂞 "
-          set -g @catppuccin_icon_window_bell "󰂞 "
-        '';
-      }
+      # {
+      #   plugin = tmuxPlugins.mkTmuxPlugin {
+      #     pluginName = "tmux-sessionx";
+      #     version = "0.1";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "omerxx";
+      #       repo = "tmux-sessionx";
+      #       rev = "e39372ad2a275d73b1680483608c4dbac5c1a219";
+      #       sha256 = "sha256-daV8D6d7JmjroYA7CBEjWGNu1KDCWafVGeYrLh8FAsg=";
+      #     };
+      #   };
+      # }
 
     ];
   };
