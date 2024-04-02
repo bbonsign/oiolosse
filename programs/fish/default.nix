99038515f1aeb52d6c51e6957a62164b3798548c
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-04-02 15:41:11 -0400

    feat: set env var to save iex/erl history
    TODO: this could probably be put in a more general spot for
    home-manager

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -31,6 +31,8 @@
       set -gx VISUAL "nvim"
       set -gx FZF_CTRL_T_COMMAND "fd --color always --follow --ignore-file '$HOME/.config/fd/ignore'"
 
+      set -gx ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
+
       fzf_configure_bindings
       set -gx fzf_preview_dir_cmd 'eza --long --all --color=always'
     '';
