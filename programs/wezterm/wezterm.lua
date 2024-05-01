4f3e0e0f762189414a6668f91fc7c4cbfba602b8
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-05-01 13:38:19 -0400

    style: add stylua.toml and format wezterm configs

--- programs/wezterm/wezterm.lua
+++ programs/wezterm/wezterm.lua
@@ -1,25 +1,25 @@
-local wezterm = require 'wezterm'
-local fonts = require 'fonts'
-local keys = require 'keys'
+local wezterm = require("wezterm")
+local fonts = require("fonts")
+local keys = require("keys")
 
 -- Show which key table is active in the status area
-wezterm.on('update-right-status', function(window, pane)
+wezterm.on("update-right-status", function(window, pane)
   local name = window:active_key_table()
   if name then
-    name = 'TABLE: ' .. name
+    name = "TABLE: " .. name
   end
-  window:set_right_status(name or '')
+  window:set_right_status(name or "")
 end)
 
 local config = {
-  color_scheme = 'tokyonight',
+  color_scheme = "tokyonight",
   -- window_background_image = '/home/bbonsign/Pictures/Wallpapers/mountain-unsplash-6-small.jpg',
   -- text_background_opacity = 1.0,
   window_background_opacity = 0.70,
   use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
   warn_about_missing_glyphs = false,
-  window_decorations = 'RESIZE',
+  window_decorations = "RESIZE",
   inactive_pane_hsb = {
     saturation = 0.8,
     brightness = 0.5,
@@ -34,7 +34,7 @@ local config = {
   },
 
   enable_kitty_keyboard = true,
-   enable_scroll_bar = true,
+  enable_scroll_bar = true,
 }
 
 for k, v in pairs(fonts) do
