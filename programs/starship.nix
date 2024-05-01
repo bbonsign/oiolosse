fbf96270bdb83dc43983382c6cb7daef031e02bc
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-05-01 13:35:37 -0400

    put starship container section in right prompt

--- programs/starship.nix
+++ programs/starship.nix
@@ -6,8 +6,7 @@
     settings = {
       add_newline = true;
       format =
-        "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$solidity$swift$terraform$typst$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$direnv$env_var$crystal$custom$sudo$cmd_duration$fill$time$line_break$jobs$battery$status$os$container$shell$character";
-      # right_format = "$time";
+        "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$solidity$swift$terraform$typst$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$direnv$env_var$crystal$custom$sudo$cmd_duration$fill$container$time$line_break$jobs$battery$status$os$shell$character";
 
       line_break.disabled = false;
       aws.disabled = true;
@@ -22,6 +21,7 @@
         success_symbol = "[λ](bold blue)";
         error_symbol = "[λ](bold red)";
       };
+      container.style = "purple";
       elixir = {
         symbol = " ";
         style = "#5e3f9e";
