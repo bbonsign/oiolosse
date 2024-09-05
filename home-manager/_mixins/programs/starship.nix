5435cae2f35a021b9de5d9f5fac059af0322a531
Brian Bonsignore <bonsignore.brian@gmail.com>
2024-09-05 14:45:50 -0400

    feat(starship): show cloud profile info

--- home-manager/_mixins/programs/starship.nix
+++ home-manager/_mixins/programs/starship.nix
@@ -7,9 +7,17 @@ _:
       settings = {
         add_newline = true;
         format =
-          "$username$hostname$localip$singularity$kubernetes$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$solidity$swift$terraform$typst$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$direnv$env_var$crystal$custom$sudo$cmd_duration$fill$container$shell$shlvl$time$line_break$jobs$battery$status$os$character";
-
-        aws.disabled = true;
+          "$username$hostname$localip$singularity$kubernetes$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$solidity$swift$terraform$typst$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$direnv$env_var$crystal$custom$sudo$cmd_duration$fill$aws$azure$gcloud$container$shell$shlvl$time$line_break$jobs$battery$status$os$character";
+        aws = {
+          symbol = " ";
+          format = "[$symbol(|$profile)(|$region)(|$duration)]($style) ";
+          region_aliases = {
+            us-east-1 = "use1";
+            us-east-2 = "use2";
+            us-west-1 = "usw1";
+            us-west-2 = "usw2";
+          };
+        };
         character = {
           success_symbol = "[λ](bold blue)";
           error_symbol = "[λ](bold red)";
@@ -22,7 +30,7 @@ _:
         };
         elm.symbol = " ";
         fill.symbol = " ";
-        gcloud.disabled = true;
+        # gcloud.disabled = true;
         git_branch.symbol = " ";
         golang.symbol = " ";
         haskell.symbol = " ";
