291ee7dbec69ef62680562e5b38a257c6f7bab74
Brian Bonsignore <bonsignore.brian@gmail.com>
2025-04-07 14:17:38 -0400

    feat(nvim): add virt-column for better looking colorcolumn

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -1,23 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Default options that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
 vim.g.maplocalleader = ";"
 vim.g.VM_leader = ";" -- for "mg979/vim-visual-multi" in plugins/multicursors.lua
 
 local opt = vim.opt
 
-opt.mousemoveevent = true
 opt.cmdheight = 0
+-- Draw vertical line one column past textwidth
+opt.colorcolumn = "+1"
 opt.conceallevel = 0
+opt.mousemoveevent = true
 opt.number = false
 opt.relativenumber = false
+opt.textwidth = 100
 
 vim.g.snacks_animate = false
-
--- set to `true` to follow the main branch
--- you need to have a working rust toolchain to build the plugin
--- in this case.
--- vim.g.lazyvim_blink_main = true
 
 -- vim.opt.foldtext = "v:lua.require'bb.foldtext'.foldtext()"
