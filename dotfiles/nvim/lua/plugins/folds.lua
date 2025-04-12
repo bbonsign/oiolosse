974ab70479bd8c20177f8c928a389e10442eaa8c
Brian Bonsignore <bonsignore.brian@gmail.com>
2025-04-12 00:10:48 -0400

    feat(nvim): add subword movement for w, e, b

--- dotfiles/nvim/lua/plugins/folds.lua
+++ dotfiles/nvim/lua/plugins/folds.lua
@@ -1,5 +1,16 @@
 return {
   "chrisgrieser/nvim-origami",
   event = "VeryLazy",
-  opts = {}, -- needed even when using default config
+  opts = {
+    foldtextWithLineCount = {
+      enabled = package.loaded["ufo"] == nil,
+      template = "    %s lines", -- `%s` gets the number of folded lines
+      hlgroupForCount = "Comment",
+    },
+
+    foldKeymaps = {
+      setup = true, -- modifies `h` and `l`
+      hOnlyOpensOnFirstColumn = true,
+    },
+  },
 }
--- dotfiles/nvim/lua/plugins/text-objects.lua
+++ dotfiles/nvim/lua/plugins/text-objects.lua
@@ -1,5 +1,23 @@
 return {
-  "Julian/vim-textobj-variable-segment",
-  event = "BufRead",
-  dependencies = "kana/vim-textobj-user",
+  {
+    "Julian/vim-textobj-variable-segment",
+    event = "BufRead",
+    dependencies = "kana/vim-textobj-user",
+  },
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    opts = {
+      -- -- defaults:
+      --   skipInsignificantPunctuation = true,
+      --   consistentOperatorPending = false, -- see "Consistent Operator-pending Mode" in the README
+      --   subwordMovement = true,
+      --   customPatterns = {}, -- check "Custom Movement Patterns" in the README for details
+    },
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+    },
+  },
 }
